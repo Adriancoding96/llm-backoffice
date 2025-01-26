d20bcf33e472cbaf1a7793535361264d9363bca9
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-26 15:45:39 +0000

    **Added** types needed for comunication with back-end

---
+++ src/lib/types.ts
@@ -0,0 +1,33 @@
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  email: string;
+  token: string;
+}
+
+export interface SignupRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface PromptRecord {
+  prompt: string;
+  uuid: string;
+}
+
+export interface PromptResponse {
+  response: string;
+  keywords: string[];
+  uuid: string;
+}
+
+export interface ResponseRecord {
+  responseBody: string;
+  metaData: string[];
+  rating: string;
+  promptUuid: string;
+}
