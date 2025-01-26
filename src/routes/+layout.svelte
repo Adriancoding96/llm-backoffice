11576313b63eef0d6a4a2316db6933f33a904471
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-26 15:19:52 +0000

    **Added** first commit containing project structure and sidebar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+  import { AppRail, AppRailTile, AppRailAnchor, LightSwitch } from "@skeletonlabs/skeleton"; 
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+</script>
+
+<AppRail class="fixed top-0 left-0 h-full w-18  surface-200 z-20">
+  <svelte:fragment slot="lead">
+    <AppRailAnchor href="/" title="Home">
+    </AppRailAnchor>
+    <AppRailAnchor href="/users" title="Users">
+    </AppRailAnchor>
+    <AppRailAnchor href="/prompts" title="Prompts">
+    </AppRailAnchor>
+    <AppRailAnchor href="/responses" title="Responses">
+    </AppRailAnchor>
+  </svelte:fragment>
+
+  <svelte:fragment slot="trail">
+    <AppRailAnchor href="/settings" title="Settings">
+    </AppRailAnchor>
+    <div class="container pl-4 pb-4">
+      <LightSwitch />
+    </div>
+  </svelte:fragment>
+</AppRail>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <h1>Hello World</h1>
+</div>
